ac4b58378ee5a7cdaebfe493aff53f144d3263a6
Brett Markland-Sanchez <Brett.MarklandSanchez@gmail.com>
2021-05-20 22:13:37 -0400

    refined style and layout, adjusted tags and id/class assignments

---
+++ Develop/script.js
@@ -0,0 +1,11 @@
+let hr9 = $('#hr9');
+let hr10 = $('#hr10');
+let hr11 = $('#hr11');
+let hr12 = $('#hr12');
+let hr1 = $('#hr1');
+let hr2 = $('#hr2');
+let hr3 = $('#hr3');
+let hr4 = $('#hr4');
+let hr5 = $('#hr5');
+
+hr10.text(`test`);
