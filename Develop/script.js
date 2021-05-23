08c9f6baa23b8bcbb96039bb331b93d4176e14db
Brett Markland-Sanchez <Brett.MarklandSanchez@gmail.com>
2021-05-22 21:34:54 -0400

    completed logic for saving notes on button click using forEach loops

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,43 @@
-let hr9 = $('#hr9');
-let hr10 = $('#hr10');
-let hr11 = $('#hr11');
-let hr12 = $('#hr12');
-let hr1 = $('#hr1');
-let hr2 = $('#hr2');
-let hr3 = $('#hr3');
-let hr4 = $('#hr4');
-let hr5 = $('#hr5');
+let currentDay = $('#currentDay');
 
-hr10.text(`test`);
+let hours = document.querySelectorAll(`textarea`);
+let buttons = document.querySelectorAll(`button`);
+
+let today = moment().format(`dddd\, MMM Do`);
+let time = moment().format(`HH`);
+
+// test out different times of day by changing 'time'
+// time = 12;
+
+currentDay.text(today);
+
+// color codes each row based on the current time using class assignment
+hours.forEach(i => {
+    let hour = i.id;
+    // remove non-number values from id name and parse integer value
+    hour = hour.replace(/[^0-9]/g,'');
+    hour = parseInt(hour);
+    // debug comparisons
+    // console.log(`The time is ${time} and the hour is ${hour}`);
+    if(hour < time){
+        i.classList.add(`past`);
+    }else if(hour > time){
+        i.classList.add(`future`);
+    }else i.classList.add(`present`);
+});
+
+buttons.forEach((i,x) => {
+    i.addEventListener("click", event => {
+        save(x);
+    });
+});
+
+// saves textarea contents on button click
+function save(x){
+    localStorage.setItem(`note${x}`, hours[x].value);
+};
+
+// restores saved notes if any are present
+hours.forEach((i,x) => {
+    i.innerText = localStorage.getItem(`note${x}`);
+})
